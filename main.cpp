889538e0553c3f6959e413c77cc526425ea240c9
zbysheck <zbyshekh@gmail.com>
2015-04-15 01:59:47 +0200

    zrobione

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(){
+    int a, b, temp, c;
+    cin >> c;
+    int tab[c];
+    for (int i = 0; i < c; i++){
+        cin >> tab[i];
+    }
+    cin >> c;
+    for (int i = 0; i < c; i++){
+        cin >> a;
+        cin >> b;
+        for (temp = 0; a <= b; ++a)
+            temp+=tab[a];
+        cout << temp << "\n";
+    }
     return 0;
 }
