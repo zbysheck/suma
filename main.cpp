89942aa4de74325bd39f9aa8265990662d6cb1a3
Zbyszek Kisły <zbyshekh@gmail.com>
2015-04-15 02:00:59 +0200

    Update main.cpp
    less code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-using namespace std;
-
 int main(){
-    int a, b, temp, c;
-    cin >> c;
-    int tab[c];
-    for (int i = 0; i < c; i++){
-        cin >> tab[i];
-    }
-    cin >> c;
-    for (int i = 0; i < c; i++){
-        cin >> a;
-        cin >> b;
-        for (temp = 0; a <= b; ++a)
-            temp+=tab[a];
-        cout << temp << "\n";
-    }
+	int a, b, temp, c;
+	std::cin >> c;
+	int tab[c];
+	for (int i = 0; i < c; i++)
+		std::cin >> tab[i];
+	std::cin >> c;
+	for (int i = 0; i < c; i++){
+		std::cin >> a >> b;
+		for (temp = 0; a <= b; ++a)
+			temp+=tab[a];
+		std::cout << temp << "\n";
+	}
     return 0;
 }
